af4f71fd652d9166d5f3194e5e03bc14dc64a2b8
Luka Furlan <luka@evriala.net>
2011-09-05 14:51:06 +0200

    utils.py: added parse_iostat function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import subprocess as sub
+import os
 
 def run(cmd):
     cmd = cmd.split()
@@ -15,6 +16,22 @@ def parse_storage(data, fields):
     parsed = {}
     # Transform data to array and remove header and an empty line at the end.
     data = data.split('\n')[1:-1]
+    for item in data:
+        item = item.split()
+        parsed[item[0]] = {}
+        for i in range(1, len(fields)+1):
+            parsed[item[0]][fields[i-1]] = item[i]
+    return parsed
+
+def parse_iostat(data, fields):
+    '''Parse iostat output on Linux.'''
+    parsed = {}
+    # Transform data to array and remove header (3 lines in Linux, 2 in FreeBSD) and an empty line at the end.
+    if os.uname()[0] == 'Linux':
+        data = data.split('\n')[3:-2]
+    if os.uname()[0] == 'FreeBSD':
+        data = data.split('\n')[2:-1]
+    print data
     for item in data:
         item = item.split()
         parsed[item[0]] = {}
