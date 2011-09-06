7b429ef0698ec0fdede753a59f1c3c4ec608ae64
Luka Furlan <luka@evriala.net>
2011-09-06 14:55:47 +0200

    uptime.py: variable rewrite fix

--- uptime.py
+++ uptime.py
@@ -10,19 +10,20 @@ class Linux:
         f = open('/proc/uptime', 'r', 0)
         self.seconds = float(f.read().split()[0])
         f.close()
-        return parser_uptime(self.seconds)
+        return utils.parser_uptime(self.seconds)
 
 class FreeBSD:
     def __init__(self, libc='/lib/libc.so.7'):
         self.seconds = 0
         self.data = None
         self.libc = libc
+        self.lib = None
 
     def get_uptime(self):
         from ctypes import Structure, cdll, byref, c_long
-        self.libc = cdll.LoadLibrary(self.libc)
+        self.lib = cdll.LoadLibrary(self.libc)
         class Data(Structure):
             _fields_ = [('sec', c_long), ('nsec', c_long)]
         self.data = Data()
-        self.libc.clock_gettime(7, byref(self.data))
+        self.lib.clock_gettime(7, byref(self.data))
         return utils.parser_uptime(self.data.sec)
