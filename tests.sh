bbd5c48941d17ddf9b3dfd02cbedd6329b9a4a1f
Luka Furlan <luka@evriala.net>
2011-09-09 10:23:34 +0200

    test all functions

---
+++ tests.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+echo "Test load module ..."
+python <<EOF
+from load import `uname`
+a = `uname`()
+print "get_uptime()"
+print a.get_uptime()
+print
+print "get_memory()"
+print a.get_memory()
+print
+print "get_swap()"
+print a.get_swap()
+print
+print "parse_uptime()"
+print a.parse_uptime()
+print
+print "parse_memory()"
+print a.parse_memory()
+print
+print "parse_swap()"
+print a.parse_swap()
+print
+EOF
+
+echo "Test storage module ..."
+python <<EOF
+from storage import `uname`
+a = `uname`()
+print "get_inodes()"
+print a.get_inodes()
+print
+print "get_fsusage()"
+print a.get_fsusage()
+print
+print "get_iostat()"
+print a.get_iostat()
+print
+print "parse_inodes()"
+print a.parse_inodes()
+print
+print "parse_fsusage()"
+print a.parse_fsusage()
+print
+print "parse_iostat()"
+print a.parse_iostat()
+print
+EOF
+
+if [ $? = 0 ]; then
+    echo "All is fine ..."
+fi
